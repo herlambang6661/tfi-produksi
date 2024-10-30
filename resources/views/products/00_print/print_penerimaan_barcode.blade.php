40230924becd75d257018c56d462adc47a0e7360
arizki07 <116191090+arizki07@users.noreply.github.com>
2024-10-30 12:54:49 +0700

    barcode

--- resources/views/products/00_print/print_penerimaan_barcode.blade.php
+++ resources/views/products/00_print/print_penerimaan_barcode.blade.php
@@ -5,6 +5,7 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 date_default_timezone_set('Asia/Jakarta');
 use Dompdf\Dompdf;
 ?>
+
 <html>
 
 <head>
@@ -12,12 +13,8 @@ use Dompdf\Dompdf;
     <title>Barcode - PT TFI (Stand Alone).</title>
     <!-- CSS files -->
     <link href="{{ url('assets/dist/css/tabler.min.css?1684106062') }}" rel="stylesheet" />
-    <link href="{{ url('assets/dist/css/tabler-flags.min.css?1684106062') }}" rel="stylesheet" />
-    <link href="{{ url('assets/dist/css/tabler-payments.min.css?1684106062') }}" rel="stylesheet" />
-    <link href="{{ url('assets/dist/css/tabler-vendors.min.css?1684106062') }}" rel="stylesheet" />
     <link href="{{ url('assets/dist/css/demo.min.css?1684106062') }}" rel="stylesheet" />
     <link href="{{ asset('assets/extentions/fontawesome/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/extentions/select2/css/select2.min.css') }}" rel="stylesheet">
     <style>
         @page {
             size: 150mm 100mm;
@@ -25,19 +22,18 @@ use Dompdf\Dompdf;
         }
 
         div.page {
-            height: 100mm;
-            width: 150mm;
             display: flex;
             flex-direction: column;
             justify-content: flex-start;
             align-items: center;
             text-align: center;
             page-break-after: always;
+            position: relative;
         }
 
         .header {
             display: flex;
-            justify-content: center;
+            justify-content: space-between;
             width: 100%;
             padding: 5px 10px;
             margin-bottom: 5px;
@@ -49,9 +45,22 @@ use Dompdf\Dompdf;
         }
 
         img {
-            max-width: 70%;
-            height: auto;
+            max-width: 90%;
+            height: 50%;
             margin-top: 5px;
+        }
+
+        .subkode {
+            font-size: 50px;
+            margin-top: 5px;
+        }
+
+        .qty {
+            position: absolute;
+            left: 10px;
+            bottom: 5px;
+            font-size: 18px;
+            text-align: left;
         }
     </style>
 </head>
@@ -60,15 +69,21 @@ use Dompdf\Dompdf;
     @foreach ($penerimaanItem as $item)
         <div class="page">
             <div class="header">
-                <div class="barcode-text">
+                <div class="barcode-text" style="flex: 1; text-align: left;">
                     <b>{{ $item->kodepenerimaan }}</b>
                 </div>
-                <div class="barcode-text">
+                <div class="barcode-text" style="flex: 1; text-align: right;">
                     <b>{{ $item->tanggal_kedatangan }}</b>
                 </div>
             </div>
             <img src="data:image/jpeg;base64,{{ DNS1D::getBarcodeJPG($item->subkode, 'C39', 4, 333) }}"
                 alt="{{ $item->subkode }}" />
+            <div class="subkode">
+                <b>{{ $item->subkode }}</b>
+            </div>
+            <div class="qty">
+                <b>QTY: 10 KG</b>
+            </div>
         </div>
     @endforeach
 </body>

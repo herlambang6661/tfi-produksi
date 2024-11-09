05910716ec1fcc04f25fa36e9b8b0e9e3da19dcc
arizki07 <116191090+arizki07@users.noreply.github.com>
2024-11-09 08:39:14 +0700

    modified:   app/Http/Controllers/_01_Datatables/Gudang/PenerimaanList.php 	modified:   resources/views/products/03_gudang/penerimaan.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -106,7 +106,7 @@ Route::controller(GudangController::class)->group(function () {
     Route::post('gudang/batal/proses', 'cancelOrder')->name('gudang.cancelOrder');
     Route::get('getPackage', 'getPackage')->name('getPackage');
     Route::POST('getdriver', 'getdriver')->name('getdriver');
-    Route::get('detail/penerimaan', 'detailPenerimaan')->name('detail.penerimaan');
+    Route::post('detail/penerimaan', 'detailPenerimaan')->name('detail.penerimaan');
     Route::POST('checkPrintQR', 'checkPrintQR')->name('checkPrintQR');
     //Scanner
     Route::get('gudang/scanner', 'scanner')->name('gudang.scanner');
